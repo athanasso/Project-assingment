299d785b5f202d3af5ec2c2cbd9dc2121139942f
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2022-09-08 13:18:05 +0300

    good enough

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
-const popup = document.querySelector('.full-screen');
+const popup = document.querySelector(".full-screen");
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
 function showPopup(){
-  popup.classList.remove('hidden');
+  popup.classList.remove("hidden");
 }
 
 function closePopup(){
-  popup.classList.add('hidden');
+  popup.classList.add("hidden");
 }
+
+navToggle.addEventListener("click", ()=> {
+  links.classList.toggle("show-links");
+});
