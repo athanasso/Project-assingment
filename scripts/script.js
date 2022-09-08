976e18f8fa7c7ec703d85dee6fd980fb533a3410
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2022-09-08 16:17:27 +0300

    good enough

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const popup = document.querySelector(".full-screen");
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
+const icon = document.querySelector(".nav-toggle .bi");
 
 function showPopup(){
   popup.classList.remove("hidden");
@@ -15,3 +16,14 @@ function closePopup(){
 navToggle.addEventListener("click", ()=> {
   links.classList.toggle("show-links");
 });
+
+function toggleIcon() {
+  if (icon.classList.contains("bi-arrow-up-square-fill")) {
+    icon.classList.remove("bi-arrow-up-square-fill");
+    icon.classList.add("bi-arrow-down-square-fill");
+  }
+  else {
+    icon.classList.remove("bi-arrow-down-square-fill");
+    icon.classList.add("bi-arrow-up-square-fill");
+  }
+}
